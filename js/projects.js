5308a6194105388e92d3c0676c9f08ac19c2c7a5
Rohald van Merode <rohald_89@live.nl>
2020-08-02 11:34:09 +0200

    created projects.js

---
+++ js/projects.js
@@ -0,0 +1,74 @@
+const project = [
+  {
+    title: "project 1",
+    description:
+      "This was the first project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS"],
+    github: "https://github.com/rohald89/FEWD-TD-unit01",
+    live: "https://rohald89.github.io/FEWD-TD-unit01/",
+    image: "images/projects/project1.jpg",
+  },
+  {
+    title: "project 2",
+    description:
+      "This was the second project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS"],
+    github: "https://github.com/rohald89/FEWD-TD-unit02",
+    live: "https://rohald89.github.io/FEWD-TD-unit02/",
+    image: "images/projects/project2.jpg",
+  },
+  {
+    title: "project 3",
+    description:
+      "This was the third project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS"],
+    github: "https://github.com/rohald89/FEWD-TD-unit03",
+    live: "https://rohald89.github.io/FEWD-TD-unit03/",
+    image: "images/projects/project3.jpg",
+  },
+  {
+    title: "project 4",
+    description:
+      "This was the fourth project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS", "Sass"],
+    github: "https://github.com/rohald89/FEWD-TD-unit04",
+    live: "https://rohald89.github.io/FEWD-TD-unit04/",
+    image: "images/projects/project4.jpg",
+  },
+  {
+    title: "project 5",
+    description:
+      "This was the fifth project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS", "jQuery", "JavaScript"],
+    github: "https://github.com/rohald89/FEWD-TD-unit05",
+    live: "https://rohald89.github.io/FEWD-TD-unit05/",
+    image: "images/projects/project5.jpg",
+  },
+  {
+    title: "project 6",
+    description:
+      "This was the sixth project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/rohald89/FEWD-TD-unit06",
+    live: "https://rohald89.github.io/FEWD-TD-unit06/",
+    image: "images/projects/project6.jpg",
+  },
+  {
+    title: "project 7",
+    description:
+      "This was the seventh project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/rohald89/FEWD-TD-unit07",
+    live: "https://rohald89.github.io/FEWD-TD-unit017",
+    image: "images/projects/project7.jpg",
+  },
+  {
+    title: "project 8",
+    description:
+      "This was the eight project of the Front End Web Development Techdegree.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/rohald89/FEWD-TD-unit08",
+    live: "https://rohald89.github.io/FEWD-TD-unit08/",
+    image: "images/projects/project8.jpg",
+  },
+];
